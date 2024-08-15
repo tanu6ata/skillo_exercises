ecbc33c446bd23d9295a6039b2d1c173ed8e6386
tanya.tsvetanova <tanya.tsvetanova@fadata.eu>
2024-08-15 18:44:46 +0300

    remove redundant calculateRevenue

--- src/skillo/lection4/SalesRevenue.java
+++ src/skillo/lection4/SalesRevenue.java
@@ -6,7 +6,7 @@ public class SalesRevenue {
 
     public static void main(String[] args) {
 
-        calculateRevenue();
+        calcSalesRevenue();
     }
 
     public static void calcSalesRevenue() {
@@ -41,41 +41,6 @@ public class SalesRevenue {
 
         System.out.println("The total revenue from sale is: " + discountedRevenue + "$");
         System.out.println("The discount from sale is: " + discountAmount + "$");
-    }
-
-    public static void calculateRevenue() {
-        double revenue;
-        double unitPrice;
-        int quantity;
-        double discountRate;
-        double discount;
-        String revenueMessage = "The revenue from sale: ";
-        String discountMessage = "Discount: ";
-        char dollar = '$';
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Please enter unit price:");
-        unitPrice = scanner.nextDouble();
-
-        System.out.println("Please enter quantity:");
-        quantity = scanner.nextInt();
-
-        revenue = unitPrice * quantity;
-
-        if (quantity >= 100 && quantity <= 120) {
-            discountRate = 0.15;
-        } else if (quantity > 120) {
-            discountRate = 0.20;
-        } else {
-            discountRate = 0;
-        }
-
-        discount = revenue * discountRate;
-        revenue = revenue - discount;
-
-        System.out.println(revenueMessage + revenue + dollar);
-        System.out.println(discountMessage + discount + dollar);
     }
 
 }
